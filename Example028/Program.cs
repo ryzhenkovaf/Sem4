553ac2a2e29d884ce6169cfcd9678f1e9c39fac5
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-21 20:35:45 +0700

    add 028

---
+++ Example028/Program.cs
@@ -0,0 +1,19 @@
+﻿//28 Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+int GetMultiply(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result *=i;
+    }
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр равно {GetMultiply(num)}");
