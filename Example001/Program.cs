81d17404e72463e8ad0abaa05f3d805edb255e5f
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-21 20:35:18 +0700

    add 024

---
+++ Example001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+int GetSum(int n){
+    int sum = 0;
+    for (int i = 1; i <=n; i++)
+    sum += i;
+    return sum;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма от 1 до {n} равна {GetSum(n)}");
+
